c3078193b33d597c6043f52d2aa22e2947ada10a
Mitchell Slager <mslager92@gmail.com>
2021-02-08 14:23:42 +0100

    fix: add correct import order

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
 import { Box, Button } from '@material-ui/core'
 import { ArrowBack } from '@material-ui/icons'
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const BackButton: React.FunctionComponent = ({ children = 'Go back' }) => {
   let history = useHistory()
