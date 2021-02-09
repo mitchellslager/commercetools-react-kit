f4cfc611f2fd3c3e761ebbdc19cd7fb70064f4bb
Mitchell Slager <mslager92@gmail.com>
2021-02-09 08:31:12 +0100

    feat: add commercetools api client

--- src/commercetools/commercetools-client.ts
+++ src/commercetools/commercetools-client.ts
@@ -12,19 +12,12 @@ const authMiddleware = createAuthMiddlewareForClientCredentialsFlow({
     clientId: process.env.CTP_CLIENT_ID,
     clientSecret: process.env.CTP_CLIENT_SECRET,
   },
-  // host: 'https://auth.europe-west1.gcp.commercetools.com',
-  // projectKey,
-  // credentials: {
-  //   clientId: 'SSAcsDVRsc80nD-UpIulM5C1',
-  //   clientSecret: 'onq1hdKUQfIVKPOb6FYBw2BzWkYBnLAT',
-  // },
   scopes: ['manage_project:react-components-kit'],
   fetch,
 })
 
 const httpMiddleware = createHttpMiddleware({
   host: process.env.CTP_API_URL,
-  // host: 'https://api.europe-west1.gcp.commercetools.com',
   fetch,
 })
 
--- src/commercetools/commercetools-requests.ts
+++ src/commercetools/commercetools-requests.ts
@@ -9,11 +9,53 @@ const key = 'order-number'
 
 type OrderNumber = { version?: number; value: number }
 
+export const searchProduct = (filter: string, sort: string) =>
+  apiRoot
+    .withProjectKey({ projectKey })
+    .productProjections()
+    .search()
+    .get({ queryArgs: { markMatchingVariants: true, sort: sort } })
+    .execute()
+    .then((r) => r.body)
+
+export const getAllCategories = () =>
+  apiRoot
+    .withProjectKey({ projectKey })
+    .categories()
+    .get()
+    .execute()
+    .then((r) => r.body)
+
+export const queryProducts = (filter: string, sort: string) =>
+  apiRoot
+    .withProjectKey({ projectKey })
+    .products()
+    .get({ queryArgs: { where: filter, sort: sort } })
+    .execute()
+    .then((r) => r.body)
+
 export const getAllProducts = () =>
   apiRoot
     .withProjectKey({ projectKey })
     .products()
     .get()
+    .execute()
+    .then((r) => r.body)
+
+export const getProductWithKey = (key: string) =>
+  apiRoot
+    .withProjectKey({ projectKey })
+    .products()
+    .withKey({ key: key })
+    .get()
+    .execute()
+    .then((r) => r.body)
+
+export const getSortedProducts = (sort: string) =>
+  apiRoot
+    .withProjectKey({ projectKey })
+    .products()
+    .get({ queryArgs: { sort: sort } })
     .execute()
     .then((r) => r.body)
 
